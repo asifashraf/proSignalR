ec8ade909fd3bb02ee5b7bb269fb7d1ef7a36530
asifashraf <asif.log@gmail.com>
2015-07-31 12:24:45 +0500

    Bootstarpped form

---
+++ Chapter 3 Code/Chapter3/Chapter3/main.js
@@ -0,0 +1,34 @@
+﻿function logg(a, b) {
+    console.log(a, b);
+}
+
+$(function () {
+    var broadcaster = $.connection.firstHub;
+    broadcaster.client.displayText = function (name, message) {
+        logg(name, message);
+        $('#messages').append('<li>' + name + ' said: ' + message + '</li>');
+    };
+
+    $.connection.hub.start().done(function () {
+        $("#join").click(function () {
+            logg("join clicked");
+            broadcaster.server.join($('#groupName').val());
+            broadcaster.state.GroupName = $('#groupName').val();
+        });
+
+        $("#leave").click(function () {
+            logg("leave clicked");
+            broadcaster.server.leave($('#groupName').val());
+            broadcaster.state.GroupName = null;
+        });
+
+
+        $("#broadcast").click(function () {
+            logg("broadcast clicked");
+            broadcaster.server.broadcastMessage({
+                Name: $('#name').val(),
+                Message: $('#message').val()
+            });
+        });
+    });
+});
